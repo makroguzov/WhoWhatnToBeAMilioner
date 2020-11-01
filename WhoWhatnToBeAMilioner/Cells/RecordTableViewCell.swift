cdbfd86f110550bdaaa4f38af8a8bf19a9503074
Valery <valera.nedbai.developer@gmail.com>
2020-11-01 15:22:16 +0300

    in progress

---
+++ WhoWhatnToBeAMilioner/Cells/RecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecordTableViewCell.swift
+//  WhoWhatnToBeAMilioner
+//
+//  Created by Валерий Макрогузов on 01.11.2020.
+//
+
+import UIKit
+
+class RecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WhoWhatnToBeAMilioner/Controllers/RecordsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecordsViewController.swift
+//  WhoWhatnToBeAMilioner
+//
+//  Created by Валерий Макрогузов on 01.11.2020.
+//
+
+import UIKit
+
+class RecordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
